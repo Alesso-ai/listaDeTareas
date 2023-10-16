bcd6bac2132f7d9bb30343f8d281d4e833d70e94
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-10-16 12:41:25 +0200

    Retocando el JS

---
+++ script/script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const taskBtn = document.getElementById("taskBtn");
+    const deleteTaskBtn = document.getElementById("deleteTaskBtn");
+    const taskInput = document.getElementById("taskInput");
+
+
+    taskBtn.addEventListener("click", addTask);
+    deleteTaskBtn.addEventListener("click", deleteTaskBtn);
+
+    /*Keyup para que reconozca tecla del teclado y le metes event
+    para al presionar enter añada  */
+
+    taskInput.addEventListener("Keyup", function (event){
+
+        /* Operador ternario añado evento de teclado igual a enter
+        meto el ? addtask() para añadir lista y le meto el null  
+        para cualquier otra tecla no haga nada */
+        (event.key=="Enter") ? addTask() : null;
+
+
+    });
+
+
+
+    function addTask() {
+
+        /*Crear elementos li con una constante y createElement*/
+
+        const li = document.createElement("li");
+        const checkbox = document.createElement("input");
+
+        /*Crear atributos para esa etiqueta*/
+
+        checkbox.type = "checkbox";
+        checkbox.className = "checkbox";
+
+
+    }
+
+    function deleteTaskBtn() {
+
+    }
+
+
+
+
+
+
+
+
+});
